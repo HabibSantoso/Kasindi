b62505aad6bd3faa32ba3bed8a172d40cea4f18b
HabibSantoso <habib.santoso.ft18@mail.umy.ac.id>
2024-01-12 03:13:31 +0700

    add tabel untuk data yang di simpan di package model

---
+++ app/src/main/java/com/example/kasindi/model/Kasindi.kt
@@ -0,0 +1,23 @@
+package com.example.kasindi.model
+
+import androidx.room.Entity
+import androidx.room.PrimaryKey
+
+@Entity(tableName = "tblUser")
+data class user(
+    @PrimaryKey(autoGenerate = true)
+    val uid : Int = 0,
+    val nama : String,
+    val email : String,
+    val isadmin : Boolean = false
+)
+
+@Entity(tableName = "tbltransaksi")
+data class transaksi(
+    @PrimaryKey(autoGenerate = true)
+    val tid : Int = 0,
+    val keterangan : String,
+    val tipe : String,
+    val nominal : Int
+)
+
