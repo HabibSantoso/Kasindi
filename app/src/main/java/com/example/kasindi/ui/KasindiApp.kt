30e619bc884896ff86829f2a3c8f93b48d35df84
HabibSantoso <habib.santoso.ft18@mail.umy.ac.id>
2024-01-12 05:06:49 +0700

    add KasindiApp.kt dan menambah string yang di butuhkan

---
+++ app/src/main/java/com/example/kasindi/ui/KasindiApp.kt
@@ -0,0 +1,50 @@
+@file:OptIn(ExperimentalMaterial3Api::class)
+
+package com.example.kasindi.ui
+
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.ArrowBack
+import androidx.compose.material3.CenterAlignedTopAppBar
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Text
+import androidx.compose.material3.TopAppBarScrollBehavior
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.stringResource
+import com.example.kasindi.R
+
+@Composable
+fun TopAppBarKasindi(
+    title: String,
+    canNavigateBack: Boolean,
+    modifier: Modifier = Modifier,
+    scrollBehavior: TopAppBarScrollBehavior? = null,
+    navigateUp: () -> Unit = {}
+) {
+    CenterAlignedTopAppBar(title = { Text(title) },
+        modifier = modifier,
+        scrollBehavior = scrollBehavior,
+        navigationIcon = {
+            if (canNavigateBack) {
+                IconButton(onClick = navigateUp) {
+                    Icon(imageVector = Icons.Filled.ArrowBack,
+                        contentDescription = ""
+                    )
+                }
+            }
+        })
+}
+
+@Composable
+fun TopAppBar(scrollBehavior: TopAppBarScrollBehavior, modifier: Modifier = Modifier) {
+    CenterAlignedTopAppBar(
+        scrollBehavior = scrollBehavior,
+        title = {
+            Text(text = stringResource(id = R.string.app_name),
+                style = MaterialTheme.typography.headlineSmall)
+        },
+        modifier = modifier)
+}
