fafad74447341ebcedb317fdc44d583748b69f68
HabibSantoso <habib.santoso.ft18@mail.umy.ac.id>
2024-01-12 03:44:59 +0700

    update DatabaseKasindi untuk menghasilkan Database di package model

--- app/src/main/java/com/example/kasindi/model/DatabaseKasindi.kt
+++ app/src/main/java/com/example/kasindi/model/DatabaseKasindi.kt
@@ -1,8 +1,23 @@
 package com.example.kasindi.model
 
+import android.content.Context
 import androidx.room.Database
+import androidx.room.Room
 import androidx.room.RoomDatabase
 
 @Database(entities = [User::class, Transaksi::class], version = 1, exportSchema = false)
 abstract class DatabaseKasindi : RoomDatabase() {
+    abstract fun kasindiDao() : KasindiDao
+
+    companion object{
+        private  var Instance: DatabaseKasindi? = null
+
+        fun getDatabase(context: Context): DatabaseKasindi{
+            return (Instance ?: synchronized(this){
+                Room.databaseBuilder(context,
+                    DatabaseKasindi::class.java,
+                    "Kasindi_Database").build().also { Instance = it }
+            })
+        }
+    }
 }
