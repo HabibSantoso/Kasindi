72736a5f9a67eb7a5f2bee4c82762c671978774b
HabibSantoso <habib.santoso.ft18@mail.umy.ac.id>
2024-01-12 04:11:59 +0700

    add RepositoriKasindi untuk mengakses data

---
+++ app/src/main/java/com/example/kasindi/repositori/RepositoriKasindi.kt
@@ -0,0 +1,27 @@
+package com.example.kasindi.repositori
+
+import com.example.kasindi.model.Transaksi
+import com.example.kasindi.model.User
+import kotlinx.coroutines.flow.Flow
+
+interface RepositoriKasindi {
+    fun getAllUserStream(): Flow<List<User>>
+
+    fun getUserStraem(uid: Int): Flow<User>
+
+    suspend fun insertU(user: User)
+
+    suspend fun deleteU(user: User)
+
+    suspend fun updateU(user: User)
+
+    fun getAllTransStream(): Flow<List<Transaksi>>
+
+    fun getTransStream(tid: Int): Flow<Transaksi>
+
+    suspend fun insertT(transaksi: Transaksi)
+
+    suspend fun deleteT(transaksi: Transaksi)
+
+    suspend fun update(transaksi: Transaksi)
+}
