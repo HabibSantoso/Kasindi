91824af404e13afc2c29bcd204bd10df414302ca
HabibSantoso <habib.santoso.ft18@mail.umy.ac.id>
2024-01-12 13:06:48 +0700

    update PengelolaHalaman dan PenyediaViewModel agar bisa menampilkan Detail transaksi

--- app/src/main/java/com/example/kasindi/navigasi/PengelolahHalaman.kt
+++ app/src/main/java/com/example/kasindi/navigasi/PengelolahHalaman.kt
@@ -4,12 +4,16 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.navigation.NavController
 import androidx.navigation.NavHostController
+import androidx.navigation.NavType
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
 import androidx.navigation.compose.rememberNavController
+import androidx.navigation.navArgument
 import com.example.kasindi.ui.view.screen.AddTransScreen
 import com.example.kasindi.ui.view.screen.DestinasiAddTran
+import com.example.kasindi.ui.view.screen.DestinasiDetailTrans
 import com.example.kasindi.ui.view.screen.DestinasiHome
+import com.example.kasindi.ui.view.screen.DetailTransScreen
 import com.example.kasindi.ui.view.screen.HomeScreen
 
 @Composable
@@ -22,10 +26,24 @@ fun PengelolaHalaman(navController: NavHostController = rememberNavController())
         composable(DestinasiHome.route) {
             HomeScreen(
                 navigateToItemEntry = { navController.navigate(DestinasiAddTran.route) },
-                onDetailClick = {})
+                onDetailClick = {
+                    navController.navigate("${DestinasiDetailTrans.route}/$it")
+                })
         }
         composable(DestinasiAddTran.route){
             AddTransScreen(navigateBack = { navController.popBackStack() })
+        }
+
+        composable(
+            DestinasiDetailTrans.routeWithArg,
+            arguments = listOf(navArgument(DestinasiDetailTrans.transIdArg){
+                type = NavType.IntType
+            })
+        ){
+            DetailTransScreen(
+                navigateBack = {navController.popBackStack()},
+                navigateToEditItem = {}
+            )
         }
     }
 }
--- app/src/main/java/com/example/kasindi/ui/PenyediaViewModel.kt
+++ app/src/main/java/com/example/kasindi/ui/PenyediaViewModel.kt
@@ -1,24 +1,30 @@
 package com.example.kasindi.ui
 
 import androidx.lifecycle.ViewModelProvider
+import androidx.lifecycle.createSavedStateHandle
 import androidx.lifecycle.viewmodel.CreationExtras
 import androidx.lifecycle.viewmodel.initializer
 import androidx.lifecycle.viewmodel.viewModelFactory
 import com.example.kasindi.AplikasiKasindi
 import com.example.kasindi.ui.view.viewmodel.AddTransViewModel
+import com.example.kasindi.ui.view.viewmodel.DetailTransViewModel
 import com.example.kasindi.ui.view.viewmodel.HomeViewModel
 
 object PenyediaViewModel {
     val Factory = viewModelFactory {
         initializer {
-            HomeViewModel(aplikasiMars().container.repositoriKasindi)
+            HomeViewModel(aplicationKasindi().container.repositoriKasindi)
         }
 
         initializer {
-            AddTransViewModel(aplikasiMars().container.repositoriKasindi)
+            AddTransViewModel(aplicationKasindi().container.repositoriKasindi)
+        }
+
+        initializer {
+            DetailTransViewModel(createSavedStateHandle(),aplicationKasindi().container.repositoriKasindi)
         }
     }
 }
 
-fun CreationExtras.aplikasiMars(): AplikasiKasindi =
+fun CreationExtras.aplicationKasindi(): AplikasiKasindi =
     (this[ViewModelProvider.AndroidViewModelFactory.APPLICATION_KEY] as AplikasiKasindi)
